cb083e09753d1ea30804a12d8483e755271c4721
wangxuege <97waxueng67@gmail.com>
2022-03-16 04:55:54 +0900

    Add style

--- src/components/PrefecturePicker.tsx
+++ src/components/PrefecturePicker.tsx
@@ -2,19 +2,33 @@ import { Prefecture } from '../App'
 
 interface Props {
   prefectures: Prefecture[]
-  onClick : (prefecture : Prefecture) => void
+  onClick: (prefecture: Prefecture) => void
 }
 
-const PrefecturePicker: React.FC<Props> = ({ prefectures , onClick }) => (
+const PrefecturePicker: React.FC<Props> = ({ prefectures, onClick }) => (
   <div>
-    <h2>都道府県</h2>
-    <ul>
+    <h2 style={{marginLeft: "20pt"}}>都道府県</h2>
+    <ul
+      style={{
+        listStyle: 'none',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'flex-start',
+        justifySelf: 'auto',
+      }}
+    >
       {prefectures.map((prefecture: Prefecture) => (
         <li key={prefecture.prefCode}>
-          <input type="checkbox" id={`${prefecture.prefCode}`}
-          onClick = {() =>onClick(prefecture)}
+          <input
+            type="checkbox"
+            id={`${prefecture.prefCode}`}
+            onClick={() => onClick(prefecture)}
+            style={{ transform: 'scale(1.65)' }}
           />
-          <label htmlFor={`${prefecture.prefCode}`}>{prefecture.prefName}</label>
+
+          <label htmlFor={`${prefecture.prefCode}`} style={{ cursor: 'pointer', margin: '0.5rem', fontSize: '1.5rem' }}>
+            {prefecture.prefName}
+          </label>
         </li>
       ))}
     </ul>
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,7 @@
 const Title = () => (
   <div>
     <header>
-      <h1>都道府県別人口グラフ</h1>
+      <h1 style={{ textAlign: 'center', backgroundColor: '#555', color: 'white' }}>都道府県別人口グラフ</h1>
     </header>
   </div>
 )
