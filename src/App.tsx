605d439ef6cd5a7b7d8af1938866b430c3e38178
wangxuege <97waxueng67@gmail.com>
2022-03-15 21:29:24 +0900

    Fetched "総人口" data

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,31 @@ export interface Prefecture {
   prefName: string
 }
 
-type Prefectures = {
+type FetchedPrefs = {
+  message: null
+  result: Prefecture[]
+}
+
+type FetchedPops = {
   message: null
   result: {
-    prefCode: number
-    prefName: string
+    boundaryYear: number
+    data:{
+      label: string
+      data:{
+        year: number
+        value: number
+      }[]
+    }[]
+  }
+}
+
+type Data = {
+  prefName: string
+  prefCode: number
+  data: {
+    year: number
+    value: number
   }[]
 }
 
@@ -20,13 +40,14 @@ const url = 'https://opendata.resas-portal.go.jp/api'
 const apiKey = process.env.REACT_APP_RESAS_APIKEY || ''
 
 const App = () => {
-  const [prefectures, setPrefectures] = useState<Prefectures | null>(null)
+  const [prefectures, setPrefectures] = useState<FetchedPrefs | null>(null)
+  const [activePrefectures, setActivePrefectures] = useState<Data[]>([])
 
   // 都道府県名を取得
   useEffect(() => {
     const fetchPrefectures = async () => {
       try {
-        const res = await axios.get<Prefectures>(`${url}/v1/prefectures`, { headers: { 'X-API-KEY': apiKey } })
+        const res = await axios.get<FetchedPrefs>(`${url}/v1/prefectures`, { headers: { 'X-API-KEY': apiKey } })
         setPrefectures(res.data)
       } catch (error) {
         alert('データ取得失敗')
@@ -35,10 +56,39 @@ const App = () => {
     void fetchPrefectures()
   }, [])
 
+  const onPrefectureClick = (prefecture: Prefecture) => {
+    const newActivePrefecture = [...activePrefectures]
+    const prefIndex = newActivePrefecture.findIndex((value) => value.prefName === prefecture.prefName)
+
+    if (prefIndex > -1) {
+      newActivePrefecture.splice(prefIndex, 1)
+      setActivePrefectures(newActivePrefecture)
+    } else {
+      const fetchPopulations = async () => {
+        try {
+          const res = await axios.get<FetchedPops>(
+            `${url}/v1/population/composition/perYear?cityCode=-&prefCode=${prefecture.prefCode}`,
+            { headers: { 'X-API-KEY': apiKey } }
+          )
+          newActivePrefecture.push({
+            prefName : prefecture.prefName,
+            prefCode : prefecture.prefCode,
+            data : res.data.result.data[0].data
+          })
+          setActivePrefectures(newActivePrefecture)
+        } catch (error) {
+          alert('データ取得失敗')
+        }
+      }
+      void fetchPopulations()
+    }
+  }
+  console.log(activePrefectures)
+
   return (
     <div>
       <Title />
-      {prefectures ? <PrefecturePicker prefectures = {prefectures.result}/> :  'Loading...'}
+      {prefectures ? <PrefecturePicker prefectures={prefectures.result} onClick={onPrefectureClick} /> : 'Loading...'}
     </div>
   )
 }
